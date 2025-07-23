5201e972fa8a5be9beed7076c9132f1ef6e931e7
Nikita Turkmen <info@nikitaturkmen.com>
2025-07-22 21:04:23 -0700

    fix api url

--- Globals.vb
+++ Globals.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Globals
-    Public Const BASE_URL As String = "http://192.168.50.182:8000/"
+    Public Const BASE_URL As String = "https://ha.cker.biz/"
     Public Const BASE_API_URL As String = BASE_URL + "api/"
     Public Shared Property hardwareId As String
 
